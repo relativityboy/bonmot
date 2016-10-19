ce4907286dd335ad3716c6ae051b143a4af59785
Donovan Walker <donovan@donovanwalker.com>
2016-10-18 23:33:32 -0500

    Added support for Collection/Child-View auto creation and managment.

--- dist/bonmot.js
+++ dist/bonmot.js
@@ -18,13 +18,112 @@ define([
     vTemp, //here because pointers are a terrible thing to waste
     uniques = {};
 
+  /**
+   * Manages a set of child-views created from a collection of models.
+   * More specifically it is intended to be auto-invoked by a parent-View
+   * to manage a collection of models that is an attribute of a parent-view's Model.
+   *
+   * It can be used on its own, but you'll need to refer to source to understand how to use it.
+   * @type {any}
+   */
+  _export.CollectionViewManager = Backbone.View.extend({
+    needsModel:false,
+    constructor:function(options) {
+      var collection = false;
+
+      if(!options.el) {
+        throw new Error('CollectionViewManager must be passed an element on construction!');
+      }
+      if(!options.childView ) {
+        throw new Error('A childView must be specified for CollectionViewManager');
+      }
+
+      this.$el = $(options.el);
+      this.$el.html('');
+      this.ChildView = options.childView;
+      this.childViews = {};
+      if(options.model) {
+        collection = options.model;
+        delete options.model;
+      }
+      if(options.collection) {
+        collection = options.collection;
+      }
+      Backbone.View.apply(this, arguments);
+
+      this.setModel(collection);
+    },
+    setModel:function(collection) {
+      if(this.collection === collection) {
+        return this;
+      }
+      if(this.collection) {
+        this.childViews.each(function(view, key) {
+          view.remove();
+          delete this.childViews[key];
+        }, this);
+        this.collection.off(null, null, this);
+      }
+      if(!collection) {
+        return this;
+      }
+      this.collection = collection;
+      this.collection.each(this.newChildView, this);
+      this.collection.on('add', function(model) {
+        this.newChildView(model);
+      }, this);
+      this.collection.on('remove', function(model) {
+        this.removeChildView(model);
+      }, this);
+      this.collection.on('sort', function(){
+        var $lastEl = false;
+        this.collection.each(function(model, i) {
+          if(i === 0) {
+            this.$el.prepend(this.childViews[model.cid].$el);
+          } else {
+            $lastEl.after(this.childViews[model.cid].$el);
+          }
+          $lastEl = this.childViews[model.cid].$el;
+        }, this);
+
+      }, this);
+    },
+    newChildView:function(model) {
+      this.childViews[model.cid] = new this.ChildView({
+        model:model
+      });
+      this.$el.append(this.childViews[model.cid].$el);
+    },
+    removeChildView:function(model) {
+      this.childViews[model.cid].off(null, null, this);
+      this.childViews[model.cid].remove();
+    },
+    remove:function() {
+      _.each(this.childViews, function(view) {
+        view.off(null,null,this);
+        view.remove();
+      }, this);
+      this.collection.off(null,null,this);
+      return Backbone.View.prototype.remove.call(this);
+    }
+  });
+
   //ROOT VIEW
   View = _export.View = Backbone.View.extend({
+    /**
+     * This is the declaration of the Model 'type' to be used with this view.
+     * It is strongly recommended if this view has child views.
+     * It is recommended that the model have BonMot.Model in its prototype chain...
+     * IE, the result of a BonMot.Model.extend() operation.
+     */
+    modelInfo:false,
+
     parent:false,
     /**
-     * Handlebars Template. Called when the view is instantiated.
+     * Handlebars Template or other 'function callable' template. Called when the view is instantiated.
+     * If model is present on construction, will also be passed a toJSON representation of the model.
      */
-    hbs:false,
+    hbs:'',
 
 
     /**
@@ -70,7 +169,13 @@ define([
       } else {
         this.options = {};
       }
-      if(typeof this.hbs === 'function') {
+      if(options.hbs) {
+        this.hbs = options.hbs;
+      }
+      if(this.hbs) {
+        if(typeof this.hbs !== 'function') {
+          this.hbs = Handlebars.compile(this.hbs);
+        }
         hbsData = (options.model && options.model.toJSON)? options.model.toJSON(): {};
         if(options.el) {
           $el = jQuery(options.el);
@@ -80,11 +185,13 @@ define([
           options.el = $el[0];
         }
         this.$el = $el;//needed for $elf & findControlElements
+
         this.findControlElements();
         this.injectUnique();
       }
 
       Backbone.View.apply(this, arguments);
+      this.$el.attr('data-v-cid', this.cid);
 
       if(this.model) {
         this.stickit();
@@ -126,11 +233,21 @@ define([
     },
 
     newChildView:function(atrName, init, model) {
+      var options = {
+        el: this.$elf(init.find)[0],
+        parentView:this
+      };
       if(model) {
-        this.childViews[atrName] = new init.view({el:this.$elf(init.find)[0], model:model, parentView:this});
-      } else {
-        this.childViews[atrName] = new init.view({el:this.$elf(init.find)[0], parentView:this});
+        options.model = model;
       }
+
+      if(this.modelInfo.prototype._setCollections[atrName]) {
+        options.childView = init.view;
+        this.childViews[atrName] = new _export.CollectionViewManager(options);
+      } else {
+        this.childViews[atrName] = new init.view(options);
+      }
+
       this.childViews[atrName].on('remove', function() {
         this.childViews[atrName].off(null, null, this);
         delete this.childViews[atrName];
@@ -370,5 +487,5 @@ define([
 
   _export.Model = DWBackbone.Model.extend({});
 
-  return _export;
+  return _.clone(_export);
 });
