7f0270f53e98235d6751d9e11f7723a7615e2bd8
Donovan Walker <donovan@donovanwalker.com>
2016-11-24 23:16:51 -0600

    Pagination & Pagination control  is working in /src for in-browser CollectionView pagination (no server calls yet). src has not been put to dist.

--- src/bonmot.js
+++ src/bonmot.js
@@ -54,106 +54,6 @@ define([
     //no-op
   };
 
-  /**
-   * Manages a set of child-views created from a collection of models.
-   * More specifically it is intended to be auto-invoked by a parent-View
-   * to manage a collection of models that is an attribute of a parent-view's Model.
-   *
-   * It can be used on its own, but you'll need to refer to source to understand how to use it.
-   * @type {any}
-   * /
-  _export.CollectionViewManager = Backbone.View.extend({
-    needsModel:false,
-    constructor:function(options) {
-      var collection = false;
-
-      if(!options.el) {
-        throw new Error('CollectionViewManager must be passed an element on construction!');
-      }
-      if(!options.childView ) {
-        throw new Error('A childView must be specified for CollectionViewManager');
-      }
-      if(options.parentView) {
-        this.parentView = options.parentView;
-      }
-
-      this.$el = $(options.el);
-      this.$el.html('');
-      this.ChildView = options.childView;
-      this.childViews = {};
-      if(options.model) {
-        collection = options.model;
-        delete options.model;
-      }
-      if(options.collection) {
-        collection = options.collection;
-      }
-      Backbone.View.apply(this, arguments);
-
-      this.setModel(collection);
-    },
-    setModel:function(collection) {
-      if(this.collection === collection) {
-        return this;
-      }
-      if(this.collection) {
-        this.childViews.each(function(view, key) {
-          view.remove();
-          delete this.childViews[key];
-        }, this);
-        this.collection.off(null, null, this);
-      }
-      if(!collection) {
-        return this;
-      }
-      this.collection = collection;
-      this.collection.each(this.newChildView, this);
-      this.collection.on('add', function(model) {
-        this.newChildView(model);
-      }, this);
-      this.collection.on('remove', function(model) {
-        this.removeChildView(model);
-      }, this);
-      this.collection.on('reset', function(models, options) {
-        _.each(options.previousModels, this.removeChildView, this);
-        this.collection.each(this.newChildView, this);
-      }, this)
-      this.collection.on('sort', function(){
-        var $lastEl = false;
-        this.collection.each(function(model, i) {
-          if(i === 0) {
-            this.$el.prepend(this.childViews[model.cid].$el);
-          } else {
-            $lastEl.after(this.childViews[model.cid].$el);
-          }
-          $lastEl = this.childViews[model.cid].$el;
-        }, this);
-
-      }, this);
-    },
-    newChildView:function(model) {
-      this.childViews[model.cid] = new this.ChildView({
-        model:model,
-        parentView:this.parentView
-      });
-      this.$el.append(this.childViews[model.cid].$el);
-    },
-    removeChildView:function(model) {
-      this.childViews[model.cid].off(null, null, this);
-      this.childViews[model.cid].remove();
-    },
-    remove:function() {
-      _.each(this.childViews, function(view) {
-        view.off(null,null,this);
-        view.remove();
-      }, this);
-      this.collection.off(null,null,this);
-      delete this.options;
-      delete this.parentView;
-      return Backbone.View.prototype.remove.call(this);
-    }
-  });*/
-
   //ROOT VIEW
   View = _export.View = Backbone.View.extend({
     /**
@@ -619,9 +519,18 @@ define([
   });
 
 
+  /**
+   * Manages a set of child-views created from a collection of models.
+   * More specifically it is intended to be auto-invoked by a parent-View
+   * to manage a collection of models that is an attribute of a parent-view's Model.
+   *
+   * It can be used on its own, but you'll need to refer to source to understand how to use it.
+   * @type {any}
+   */
   CollectionView = _export.CollectionView = _export.View.extend({
     Model:DWBackbone.Collection,
     firstPage:0,
+    bindings:{'.w-atr-page':'page'},
     constructor:function(options) {
       xx = this;
       var collection = false;
@@ -663,9 +572,14 @@ define([
       //we are not going to call View.constructor if we can at all avoid it.
       //_export.View.apply(this, arguments);
       Backbone.View.apply(this, arguments);
+      this.stickit();
+
+      this.$collection = this.$elf('.w-collection:first');
+      if(this.$collection.length === 0) {
+        this.$collection = this.$el;
+      }
       this.setModel(collection);
     },
-    //BEGIN COPIED CODE
     setModel:function(collection) {
       if(this.collection === collection) {
         return this;
@@ -725,7 +639,7 @@ define([
 
       collection.each(function(model, i) {
         if(i === 0) {
-          this.$el.prepend(this.childViews[model.cid].$el);
+          this.$collection.prepend(this.childViews[model.cid].$el);
         } else {
           $lastEl.after(this.childViews[model.cid].$el);
         }
@@ -737,7 +651,7 @@ define([
         model:model,
         parentView:this.parentView
       });
-      this.$el.append(this.childViews[model.cid].$el);
+      this.$collection.append(this.childViews[model.cid].$el);
     },
     removeChildView:function(cid) {
       this.childViews[cid].off(null, null, this);
@@ -754,17 +668,6 @@ define([
       delete this.parentView;
       return Backbone.View.prototype.remove.call(this);
     },
-    //END COPIED CODE
-    /*updateViewableCollection:function() {
-      var fullCollection = this.model.get('fullCollection'),
-        collection = this.model.get('collection'),
-        sortBy = this.model.get('sortBy')
-
-      collection.comparator = sortBy;
-      fullCollection.comparator = sortBy;
-      fullCollection.sort();
-      collection.reset(fullCollection.slice(this.model.get('page'), this.model.get('pageLength') - 1));
-    },*/
     ctrlSortBy:function(evt) {
       this.model.set('sortBy', $(evt.currentTarget).data('sort-by'));
     },
@@ -781,14 +684,14 @@ define([
     },
     ctrlNext:function() {
       var page = this.model.get('page');
-      if(page < Math.floor(this.model.get('fullCollection').length / this.model.get('pageLength'))) {
+      if(page < (Math.floor(this.collection.length / this.model.get('pageLength')) - 1)) {
         this.model.set({'page': (page+1)});
       } else {
         this.ctrlLast();
       }
     },
     ctrlLast:function() {
-      this.model.set({'page': Math.floor(this.model.get('fullCollection').length / this.model.get('pageLength'))});
+      this.model.set({'page': (Math.floor(this.collection.length / this.model.get('pageLength')) - 1)});
     }
   });
 
