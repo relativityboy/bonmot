675f5db180d2e9fae3b8e5453a1371aeb0b60675
Donovan Walker <donovan@donovanwalker.com>
2017-04-27 22:35:16 -0500

    TPLAGNOSTIC - fixed hbs dependency in tests

---
+++ test/resources/hbs_libs/hbs.js
@@ -0,0 +1,29 @@
+define(["handlebars"], function (Handlebars) {
+  Handlebars = Handlebars || this.Handlebars;
+  var templateExtension = ".hbs";
+
+  return {
+
+    pluginBuilder: "./hbs-builder",
+
+    // http://requirejs.org/docs/plugins.html#apiload
+    load: function (name, parentRequire, onload, config) {
+
+      // Get the template extension.
+      var ext = (config.hbs && config.hbs.templateExtension ? config.hbs.templateExtension : templateExtension);
+
+      // Get the base path.
+      var base = (config.hbs && config.hbs.base ? config.hbs.base : false);
+      var path = ((base)? base + '/' : '') + name;
+
+      // In browsers use the text-plugin to the load template. This way we
+      // don't have to deal with ajax stuff
+      parentRequire(["text!" + path + ext], function (raw) {
+        // Just return the compiled template
+        onload(Handlebars.compile(raw));
+      });
+
+    }
+
+  };
+});
